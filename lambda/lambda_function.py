47d07520010284fbe43d0a8b9fad3673d34b6f30
Matthew Hanson <matt.a.hanson@gmail.com>
2019-01-07 00:06:07 -0500

    update lambda handler

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,13 +1,13 @@
 import boto3
 import json
 import logging
-import os
-import requests
 import sys
+
+import os.path as op
 
 from datetime import datetime
 from satstac import STACError, Collection
-from satstac.sentinel import transform, SETTINGS
+from satstac.sentinel import transform, SETTINGS, get_metadata
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -22,22 +22,20 @@ sns_arn = 'arn:aws:sns:eu-central-1:552188055668:sentinel-stac'
 
 
 def lambda_handler(event, context):
+    logger.info('Event: %s' % json.dumps(event))
     collection = Collection.open('https://sentinel-stac.s3.amazonaws.com/sentinel-2-l1c/catalog.json')
     
     msg = json.loads(event['Records'][0]['Sns']['Message'])
-    for m in msg['Records']:
-        logger.info('Message: %s' % json.dumps(m))
-        url = 'https://%s.s3.amazonaws.com/%s' % (m['s3']['bucket']['name'], m['s3']['object']['key'])
-        #id = os.path.splitext(os.path.basename(os.path.dirname(url)))[0]
-        data = {
-            #'id': id,
-            #'datetime': datetime.strptime(id.split('_')[3], '%Y%m%d'),
-            'url': url
-        }
+    logger.debug('Message: %s' % json.dumps(msg))
+
+    for m in msg['tiles']:
+        url = op.join(SETTINGS['roda_url'], m['path'], 'tileInfo.json')
+        metadata = get_metadata(url)
+        logger.debug('Metadata: %s' % json.dumps(metadata))
         # transform to STAC
-        item = transform(data)
-        logger.debug('Item: %s' % json.dumps(item.data))
-        #collection.add_item(item, path=SETTINGS['path_pattern'])
-        #logger.info('Added %s as %s' % (item, item.filename))
-        #client.publish(TopicArn=sns_arn, Message=json.dumps(item.data))
-        #logger.info('Published to %s' % sns_arn)
+        item = transform(metadata)
+        logger.info('Item: %s' % json.dumps(item.data))
+        collection.add_item(item, path=SETTINGS['path_pattern'])
+        logger.info('Added %s as %s' % (item, item.filename))
+        client.publish(TopicArn=sns_arn, Message=json.dumps(item.data))
+        logger.info('Published to %s' % sns_arn)
