7d3279a87672a710754823556b71e27f3496c384
Matthew Hanson <matt.a.hanson@gmail.com>
2019-11-21 23:37:32 -0500

    update cli module

--- stac_sentinel/__init__.py
+++ stac_sentinel/__init__.py
@@ -4,3 +4,5 @@ from stac_sentinel.sentinel import SentinelSTAC
 
 # quiet loggers
 logging.getLogger('botocore').propagate = False
+logging.getLogger('urllib3').propagate = False
+logging.getLogger('requests').propagate = False
--- stac_sentinel/cli.py
+++ stac_sentinel/cli.py
@@ -9,10 +9,6 @@ from os import makedirs, path as op
 from .sentinel import SentinelSTAC
 from .version import __version__
 
-# quiet loggers
-logging.getLogger('urllib3').propagate = False
-logging.getLogger('requests').propagate = False
-
 logger = logging.getLogger(__name__)
 
 
@@ -30,20 +26,18 @@ def parse_args(args):
     pparser.add_argument('--start_date', help='Only ingest scenes with a Last Modified Date past provided start date', default=None)
     pparser.add_argument('--end_date', help='Only ingest scenes with a Last Modified Date before provided end date', default=None)
 
-
     # add subcommands
     subparsers = parser0.add_subparsers(dest='command')
 
     # command 1
     parser = subparsers.add_parser('ingest', parents=[pparser], help='Ingest Sentinel STAC', formatter_class=dhf)
     parser.add_argument('--save', help='Save fetch Items as <id>.json files to this folder', default=None)
-
     #parser.add_argument('--publish', help='ARN to publish new Items to', default=None)
 
     # command 2
-    h = 'Get latest inventory of Sentinel metadata files'
-    parser = subparsers.add_parser('inventory', parents=[pparser], help=h, formatter_class=dhf)
-    parser.add_argument('--filename', help='Filename to save', default=str(datetime.now().date()) + '.csv')
+    #h = 'Get latest inventory of Sentinel metadata files'
+    #parser = subparsers.add_parser('inventory', parents=[pparser], help=h, formatter_class=dhf)
+    #parser.add_argument('--filename', help='Filename to save', default=str(datetime.now().date()) + '.csv')
 
     # turn Namespace into dictinary
     parsed_args = vars(parser0.parse_args(args))
@@ -75,18 +69,7 @@ def cli():
         #    records = sentinel.latest_inventory()
         #sentinel.add_items(cat, records, start_date=args['start'], end_date=args['end'],
         #                    prefix=args['prefix'], s3meta=args['s3meta'], publish=args['publish'])
-    elif cmd == 'inventory':
-        filename = args.pop('filename', None)
-        inventory = latest_inventory(**args)
-        if filename is not None:
-            # save the inventory results
-            with open(filename, 'w') as f:
-                keys = None
-                for inv in inventory:
-                    if keys is None:
-                        keys = inv.keys()
-                        f.write(','.join(keys) + '\n')
-                    f.write(','.join([str(inv[k]) for k in keys]) + '\n')
+
 
 
 if __name__ == "__main__":
