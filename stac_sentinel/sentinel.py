07eb79aa4e6cbb0e21a7e628dde9354f744f9ad0
Matthew Hanson <matt.a.hanson@gmail.com>
2020-02-19 00:26:56 -0500

    update sentinel-s2-l2a to include more files and fixes for STAC 0.9

--- stac_sentinel/sentinel.py
+++ stac_sentinel/sentinel.py
@@ -229,29 +229,35 @@ class SentinelSTAC(object):
     def to_stac_from_s2(self, base_url=''):
         """ Create STAC Item from Sentinel-2 L1C or L2A metadata """
         dt = parse(self.metadata['timestamp'])
-        # Item properties
-        props = {
-            'datetime': dt.isoformat(),
-            'platform': 'sentinel-2%s' % self.metadata['productName'][2].lower(),
-            'eo:cloud_cover': float(self.metadata['cloudyPixelPercentage']),
-            'sentinel:utm_zone': self.metadata['utmZone'],
-            'sentinel:latitude_band': self.metadata['latitudeBand'],
-            'sentinel:grid_square': self.metadata['gridSquare'],
-            'sentinel:sequence': self.metadata['path'].split('/')[-1],
-            'sentinel:product_id': self.metadata['productName']
-        }
 
         # geometry - TODO see about getting this from a productInfo file without having to reproject
         epsg = self.metadata['tileOrigin']['crs']['properties']['name'].split(':')[-1]
-        coordinates = self.metadata['tileDataGeometry']['coordinates']
-        ys = [c[1] for c in coordinates[0]]
-        xs = [c[0] for c in coordinates[0]]
+        native_coordinates = self.metadata['tileDataGeometry']['coordinates']
+        ys = [c[1] for c in native_coordinates[0]]
+        xs = [c[0] for c in native_coordinates[0]]
         p1 = Proj(init='epsg:%s' % epsg)
         p2 = Proj(init='epsg:4326')
         lons, lats = reproj(p1, p2, xs, ys)
         bbox = [min(lons), min(lats), max(lons), max(lats)]
         coordinates = [[[lons[i], lats[i]] for i in range(0, len(lons))]]
         geom = geometry.mapping(geometry.Polygon(coordinates[0]).convex_hull)
+
+        # Item properties
+        props = {
+            'datetime': dt.isoformat(),
+            'platform': 'sentinel-2%s' % self.metadata['productName'][2].lower(),
+            'eo:cloud_cover': float(self.metadata['cloudyPixelPercentage']),
+            'proj:epsg': epsg,
+            'proj:geometry': {
+                'type': 'Polygon',
+                'coordinates': native_coordinates
+            },
+            'sentinel:utm_zone': self.metadata['utmZone'],
+            'sentinel:latitude_band': self.metadata['latitudeBand'],
+            'sentinel:grid_square': self.metadata['gridSquare'],
+            'sentinel:sequence': self.metadata['path'].split('/')[-1],
+            'sentinel:product_id': self.metadata['productName']
+        }
 
         # assets
         assets = self.get_collection()['assets']
@@ -279,22 +285,25 @@ class SentinelSTAC(object):
             assets['thumbnail']['href'] = op.join(s1_base_url, 'preview.jpg')
             assets['info']['href'] = op.join(base_url, 'tileInfo.json')
             assets['metadata']['href'] = op.join(base_url, 'metadata.xml')
-            assets['overview']['href'] = op.join(base_url, 'R10m/TCI.jp2')
+            assets['overview']['href'] = op.join(base_url, 'qi/L2A_PVI.jp2')
+            assets['visual']['href'] = op.join(base_url, 'R10m/TCI.jp2')
             assets['B02']['href'] = op.join(base_url, 'R10m/B02.jp2')
             assets['B03']['href'] = op.join(base_url, 'R10m/B03.jp2')
             assets['B04']['href'] = op.join(base_url, 'R10m/B04.jp2')
             assets['B08']['href'] = op.join(base_url, 'R10m/B08.jp2')
-            assets['overview_20m']['href'] = op.join(base_url, 'R20m/TCI.jp2')
+            assets['AOT']['href'] = op.join(base_url, 'R60m/AOT.jp2')
+            assets['WVP']['href'] = op.join(base_url, 'R10m/WVP.jp2')
+            assets['visual_20m']['href'] = op.join(base_url, 'R20m/TCI.jp2')
             assets['B05']['href'] = op.join(base_url, 'R20m/B05.jp2')
             assets['B06']['href'] = op.join(base_url, 'R20m/B06.jp2')
             assets['B07']['href'] = op.join(base_url, 'R20m/B07.jp2')
             assets['B8A']['href'] = op.join(base_url, 'R20m/B8A.jp2')
             assets['B11']['href'] = op.join(base_url, 'R20m/B11.jp2')
             assets['B12']['href'] = op.join(base_url, 'R20m/B12.jp2')
-            assets['overview_60m']['href'] = op.join(base_url, 'R60m/TCI.jp2')
+            assets['SCL']['href'] = op.join(base_url, 'R20m/SCL.jp2')
+            assets['visual_60m']['href'] = op.join(base_url, 'R60m/TCI.jp2')
             assets['B01']['href'] = op.join(base_url, 'R60m/B01.jp2')
             assets['B09']['href'] = op.join(base_url, 'R60m/B09.jp2')
-            assets['B10']['href'] = op.join(base_url, 'R60m/B10.jp2')
         else:
             raise Exception(f"Collection {self.collection} not supported")
         #if dt < datetime(2016,12,6):
@@ -308,7 +317,7 @@ class SentinelSTAC(object):
         item = {
             'type': 'Feature',
             'stac_version': self.stac_version,
-            'stac_extensions': ['dtr', 'sat', 'eo'],            
+            'stac_extensions': ['sat', 'eo', 'view'],
             'id': id,
             'collection': self.collection,
             'bbox': bbox,
